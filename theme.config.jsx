04baa81bb43782fe12de9b721cec8aab6e6844dc
Levi <levi@technomancy.dev>
2024-07-24 17:17:51 +0200

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,18 @@
+export default {
+  logo: <span className="font-futura">00</span>,
+  project: {
+    link: "https://github.com/technomancy-dev/00",
+  },
+  docsRepositoryBase: "https://github.com/technomancy-dev/00-docs",
+  footer: false
+    // text: (
+    //   <span>
+    //     {new Date().getFullYear()} ©{' '}
+    //     <a href="https://double-zero.cloud" target="_blank">
+    //     00
+    //     </a>        
+    //   </span>
+    // )
+  // }
+  // ... other theme options
+};
